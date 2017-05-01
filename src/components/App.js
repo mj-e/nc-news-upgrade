0727f5320a3363cddbd4484cc9b69e3aaef65d6f
mj-e <matt_evans30@yahoo.com>
2017-05-01 10:43:49 +0100

    initial setup complete, rendering H1 tag

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1> NC news upgrade with bootstrap </h1>
+            </div>
+        );
+    }
+}
+
+//export default App;
+
+
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import '../bootstrap-3.3.7/css/bootstrap.min.css';
+
+import App from './components/App';
+
+ReactDOM.render(
+
+    <App />
+    , document.getElementById('app'));
