c060600dc5b3bae0266c98a3f4817e08f0eff833
mj-e <matt_evans30@yahoo.com>
2017-05-10 09:57:52 +0100

    added background color to app and styling to article card

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -6,7 +6,7 @@ const ArticleCard = function (props) {
     return (
         <div className='well'>
             <style>{css}</style>
-            <h4 className='h4'><Link to={`/${props.belongs_to}/${props._id}`} className='title is-3'>{props.title}</Link></h4>
+            <h4 className='h4'><Link to={`/${props.belongs_to}/${props._id}`} className='h4link'>{props.title}</Link></h4>
             <p>By {props.created_by} | Comments: {props.comments} | Category: {firstLetterCapital(props.belongs_to)}</p>
             <VoteButtons votes={props.votes} handleClick={props.voteArticle} />
         </div>
@@ -21,16 +21,16 @@ export default ArticleCard;
 
 const css = `
     .well {
-        background-color: #ffffcc;
+        background-color: #ffffd9;
     }
-    .h4{
+    .h4link{
         font-family: Optima;
-        color: #2c7fb8;
+        color: #0c2c84;
     }
     p{
-        color: #2c7fb8;
+        color: #0c2c84;
         font-family: Optima;
-        font-style: bold;
+        font-style: italic;
     }`;
 
 function firstLetterCapital(str) {
