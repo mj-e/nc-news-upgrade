8c2411cbbf314f9bd69a053a2d2a0eadd5ee7ce5
mj-e <matt_evans30@yahoo.com>
2017-05-16 13:08:48 +0100

    articles reducers tested

--- spec/articles.reducers.spec.js
+++ spec/articles.reducers.spec.js
@@ -36,4 +36,57 @@ describe('articles.reducer', () => {
         const newState = reducer(initialState);
         expect(newState).to.eql(initialState);
     });
+    describe('when passed action FETCH_ARTICLE_REQUEST', () => {
+        it('does not mutate the initial state', () => {
+            const action = actions.fetchArticlesRequest();
+            const newState = reducer(initialState, action);
+            expect(newState).to.not.equal(initialState);
+        });
+        it('sets loading to true', () => {
+            const action = actions.fetchArticlesRequest();
+            const newState = reducer(initialState, action);
+            expect(newState.loading).to.equal(true);
+        });
+    });
+    describe('when passed action FETCH_ARTICLE_SUCESS', () => {
+        it('does not mutate the initial state', () => {
+            const action = actions.fetchArticlesSuccess(initialState.articles);
+            const newState = reducer(initialState, action);
+            expect(newState).to.not.equal(initialState);
+        });
+        it('sets loading to false', () => {
+            const action = actions.fetchArticlesSuccess(initialState.articles);
+            const newState = reducer(initialState, action);
+            expect(newState.loading).to.equal(false);
+        });
+    });
+    describe('when passed action FETCH_ARTICLE_ERROR', () => {
+        it('does not mutate the initial state', () => {
+            const action = actions.fetchArticlesError();
+            const newState = reducer(initialState, action);
+            expect(newState).to.not.equal(initialState);
+        });
+        it('sets loading to false', () => {
+            const action = actions.fetchArticlesError(initialState.articles);
+            const newState = reducer(initialState, action);
+            expect(newState.loading).to.equal(false);
+        });
+    });
+    describe('when passed action VOTE_ARTICLE_SUCCESS', () => {
+        it('does not mutate the initial state', () => {
+            const action = actions.voteArticleSuccess({ _id: '591ae024a6159600047aac8e', vote: 'up' });
+            const newState = reducer(initialState, action);
+            expect(newState).to.not.equal(initialState);
+        });
+        it('votes down specific article', () => {
+            const action = actions.voteArticleSuccess({ _id: '591ae024a6159600047aac8e', vote: 'down' });
+            const newState = reducer(initialState, action);
+            expect(newState.byId['591ae024a6159600047aac8e'].votes).to.equal(5);
+        });
+        it('votes up specific article', () => {
+            const action = actions.voteArticleSuccess({ _id: '591ae024a6159600047aac8e', vote: 'up' });
+            const newState = reducer(initialState, action);
+            expect(newState.byId['591ae024a6159600047aac8e'].votes).to.equal(7);
+        });
+    });
 });
